9b910cb97c4b9a1d8d44ef6fc31880abdb18fbde
Ryan C. Durham <ryan@stagerightlabs.com>
2023-04-19 20:02:01 -0700

    Update umami tracking code

--- resources/views/show.php
+++ resources/views/show.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,11 +14,11 @@
     <link rel="manifest" href="icon/site.webmanifest">
 
     <!-- Facebook Open Graph Tags -->
-    <meta property="og:title" content="Performance Report Generator"/>
-    <meta property="og:image" content="icon/apple-touch-icon.png"/>
-    <meta property="og:site_name" content="Performance Report Generator"/>
-    <meta property="og:description" content="Stage Managers: You will never have to write a performance report again with this handy performance report generator."/>
-    <meta property="og:url" content="http://prg.stagerightlabs.com"/>
+    <meta property="og:title" content="Performance Report Generator" />
+    <meta property="og:image" content="icon/apple-touch-icon.png" />
+    <meta property="og:site_name" content="Performance Report Generator" />
+    <meta property="og:description" content="Stage Managers: You will never have to write a performance report again with this handy performance report generator." />
+    <meta property="og:url" content="http://prg.stagerightlabs.com" />
 
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Performance Report Generator</title>
@@ -34,58 +35,64 @@
     <![endif]-->
 
     <!-- Umami -->
-    <script async defer
-        data-website-id="12087a60-39b0-4639-bbcb-5e0cd0529f4f"
-        src="https://umami.stagerightlabs.com/umami.js"
-        data-do-not-track="true"
-    ></script>
-  </head>
-  <body>
+    <script async defer data-website-id="12087a60-39b0-4639-bbcb-5e0cd0529f4f" src="https://umami.stagerightlabs.com/script.js" data-do-not-track="true"></script>
+</head>
+
+<body>
     <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <div class="page-header">
-              <div class='btn-toolbar pull-right'>
-                <a class='btn btn-default btn-lg' href="/">Refresh</a>
-              </div>
-              <h1>Performance Report Generator</h1>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="page-header">
+                    <div class='btn-toolbar pull-right'>
+                        <a class='btn btn-default btn-lg' href="/">Refresh</a>
+                    </div>
+                    <h1>Performance Report Generator</h1>
+                </div>
             </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <div class="panel panel-default panel-report">
-            <div class="panel-body">
-              <?php echo $report; ?>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="panel panel-default panel-report">
+                    <div class="panel-body">
+                        <?php echo $report; ?>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <p style="text-align: center">
-            <small>
-            Crafted by Ryan Durham @ <a href="http://stagerightlabs.com" target="_blank">Stage Right Labs</a>.
-            </small>
-          </p>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <p style="text-align: center">
+                    <small>
+                        Crafted by Ryan Durham @ <a href="http://stagerightlabs.com" target="_blank">Stage Right Labs</a>.
+                    </small>
+                </p>
+            </div>
         </div>
-      </div>
     </div>
 
     <!-- Piwik -->
     <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//stats.stagerightlabs.com/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 12]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
+        var _paq = _paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u = "//stats.stagerightlabs.com/";
+            _paq.push(['setTrackerUrl', u + 'piwik.php']);
+            _paq.push(['setSiteId', 12]);
+            var d = document,
+                g = d.createElement('script'),
+                s = d.getElementsByTagName('script')[0];
+            g.type = 'text/javascript';
+            g.async = true;
+            g.defer = true;
+            g.src = u + 'piwik.js';
+            s.parentNode.insertBefore(g, s);
+        })();
     </script>
-    <noscript><p><img src="//stats.stagerightlabs.com/piwik.php?idsite=12" style="border:0;" alt="" /></p></noscript>
+    <noscript>
+        <p><img src="//stats.stagerightlabs.com/piwik.php?idsite=12" style="border:0;" alt="" /></p>
+    </noscript>
     <!-- End Piwik Code -->
-  </body>
+</body>
+
 </html>
