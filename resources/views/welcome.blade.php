1babb0ddfb72c65a4ccf5e452fc6a42c3328ea2c
Ryan C. Durham <ryan@stagerightlabs.com>
2025-11-18 13:57:24 -0800

    Switch to seline for analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,24 @@
     <meta name="description" content="Stage Managers: You will never have to write a performance report again with this handy performance report generator.">
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="icon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
-    <link rel="manifest" href="icon/site.webmanifest">
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="{{ asset("icon/apple-touch-icon.png") }}"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="{{ asset("icon/favicon-32x32.png") }}"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="{{ asset("icon/favicon-16x16.png") }}"
+    />
+    <link rel="manifest" href="{{ asset("icon/site.webmanifest") }}" />
 
     <!-- Open Graph Tags -->
     <meta property="og:title" content="Performance Report Generator" />
@@ -25,6 +39,9 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/theme.css" rel="stylesheet">
+
+    <!-- Seline -->
+    <script async src="https://cdn.seline.com/seline.js" data-token="39601abb2630291"></script>
 
 </head>
 
