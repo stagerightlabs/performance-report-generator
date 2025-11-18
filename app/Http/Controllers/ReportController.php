570eba63a6ff05e113aaced759e4c7d02c84581b
Ryan C. Durham <ryan@stagerightlabs.com>
2025-11-18 13:30:02 -0800

    Migrate from lumen to laravel

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,108 +9,95 @@ use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
-    public function __construct(ReportGenerator $reporter)
-    {
-        $this->reporter = $reporter;
-    }
+    public function __construct(protected ReportGenerator $reporter) {}
+
     /**
      * Show a generated performance report.
-     *
-     * @param  int  $id
-     * @return Response
      */
-    public function showReport()
+    public function show()
     {
         // Generate a report
         $report = $this->reporter->generate();
 
-        return view('show', ['report' => $report]);
+        return view('welcome', ['report' => $report]);
     }
 
-    /**
-     * Show the donation form
-     *
-     * @return Response
-     */
-    public function donationForm()
-    {
-        return view('form');
-    }
 
-    /**
-     * Handle the submission of a donation
-     *
-     * @param  Request $request
-     * @return Response
-     */
-    public function processDonation(Request $request)
-    {
-        // Check the ReCaptcha Field
-        $client = new Client();
-        $response = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
-            'form_params' => [
-                'secret' => env('RECAPTCHA_KEY'),
-                'response' => $request->get('g-recaptcha-response'),
-                'remoteip' => $request->ip()
-            ]
-        ]);
-        $result = json_decode($response->getBody()->getContents())->success;
 
-        if (!$result) {
-            return redirect('/error');
-        }
+    // /**
+    //  * Handle the submission of a donation
+    //  *
+    //  * @param  Request $request
+    //  * @return Response
+    //  */
+    // public function processDonation(Request $request)
+    // {
+    //     // Check the ReCaptcha Field
+    //     $client = new Client();
+    //     $response = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+    //         'form_params' => [
+    //             'secret' => env('RECAPTCHA_KEY'),
+    //             'response' => $request->get('g-recaptcha-response'),
+    //             'remoteip' => $request->ip()
+    //         ]
+    //     ]);
+    //     $result = json_decode($response->getBody()->getContents())->success;
 
-        // Process the submitted text
-        $text = e($request->get('content'));
+    //     if (!$result) {
+    //         return redirect('/error');
+    //     }
 
-        // Is there any content to work with?
-        if (empty($text)) {
-            return redirect('/donate');
-        }
+    //     // Process the submitted text
+    //     $text = e($request->get('content'));
 
-        $text = $this->textFilter($text);
-        $wordCount = str_word_count($text);
-        $sentences = explode('.', $text);
-        $sentences = array_filter(array_map('trim', $sentences));
-        $sentenceCount = count($sentences);
+    //     // Is there any content to work with?
+    //     if (empty($text)) {
+    //         return redirect('/donate');
+    //     }
 
-        // Split out the sentences
-        $firstSentence = array_shift($sentences);
-        $lastSentence = array_pop($sentences);
+    //     $text = $this->textFilter($text);
+    //     $wordCount = str_word_count($text);
+    //     $sentences = explode('.', $text);
+    //     $sentences = array_filter(array_map('trim', $sentences));
+    //     $sentenceCount = count($sentences);
 
-        // Save the first sentence
-        if ($firstSentence) {
-            $this->saveSentence($firstSentence, 'beginning');
-        }
+    //     // Split out the sentences
+    //     $firstSentence = array_shift($sentences);
+    //     $lastSentence = array_pop($sentences);
 
-        // Save the last sentence
-        if ($lastSentence) {
-            $this->saveSentence($lastSentence, 'end');
-        }
+    //     // Save the first sentence
+    //     if ($firstSentence) {
+    //         $this->saveSentence($firstSentence, 'beginning');
+    //     }
 
-        // Save the middle sentences
-        foreach ($sentences as $sentence) {
-            $this->saveSentence($sentence, 'middle');
-        }
+    //     // Save the last sentence
+    //     if ($lastSentence) {
+    //         $this->saveSentence($lastSentence, 'end');
+    //     }
 
-        return view('success', ['wordCount' => $wordCount, 'sentenceCount' => $sentenceCount]);
-    }
+    //     // Save the middle sentences
+    //     foreach ($sentences as $sentence) {
+    //         $this->saveSentence($sentence, 'middle');
+    //     }
 
-    protected function saveSentence($sentence, $type)
-    {
-        DB::table('sentences')->insert([
-            'text' => $sentence,
-            'type' => $type,
-            'word_count' => str_word_count($sentence)
-        ]);
-    }
+    //     return view('success', ['wordCount' => $wordCount, 'sentenceCount' => $sentenceCount]);
+    // }
 
-    protected function textFilter($sentence)
-    {
-        $filtered = str_replace('Mr.', 'Mr&#46;', $sentence);
-        $filtered = str_replace('Ms.', 'Ms&#46;', $filtered);
-        $filtered = str_replace('Mrs.', 'Mrs&#46;', $filtered);
+    // protected function saveSentence($sentence, $type)
+    // {
+    //     DB::table('sentences')->insert([
+    //         'text' => $sentence,
+    //         'type' => $type,
+    //         'word_count' => str_word_count($sentence)
+    //     ]);
+    // }
 
-        return $filtered;
-    }
+    // protected function textFilter($sentence)
+    // {
+    //     $filtered = str_replace('Mr.', 'Mr&#46;', $sentence);
+    //     $filtered = str_replace('Ms.', 'Ms&#46;', $filtered);
+    //     $filtered = str_replace('Mrs.', 'Mrs&#46;', $filtered);
+
+    //     return $filtered;
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
- */
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReportController;
 
-$router->get('/error', function () {
-    return view('error');
-});
-
-$router->get('/donate', ['uses' => 'ReportController@donationForm', 'as' => 'donation.form']);
-$router->post('/donate', ['uses' => 'ReportController@processDonation', 'as' => 'donation.handle']);
-$router->get('/', ['uses' => 'ReportController@showReport', 'as' => 'show']);
+Route::get('/', [ReportController::class, 'show']);
